663df0b9267c4322876de467578728e9df7e6795
programmeranamul <programmer.anamul@gmail.com>
2025-11-25 13:43:26 +0600

    environment setup

---
+++ src/components/Background.jsx
@@ -0,0 +1,29 @@
+import { Environment, Sphere } from "@react-three/drei";
+import { Gradient, LayerMaterial } from "lamina";
+import React from "react";
+import * as THREE from "three";
+
+function Background() {
+  return (
+    <>
+      {/* <Environment preset="sunset" /> */}
+      <Sphere scale={[100, 100, 100]}>
+        <LayerMaterial
+          lighting="physical"
+          side={THREE.BackSide}
+          transmission={1}
+        >
+          <Gradient
+            colorA={"#357ca1"}
+            colorB={"white"}
+            axes={"y"}
+            start={0}
+            end={-0.5}
+          />
+        </LayerMaterial>
+      </Sphere>
+    </>
+  );
+}
+
+export default Background;
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,8 +1,15 @@
 import { OrbitControls } from "@react-three/drei";
+import Background from "./Background";
+import { Suspense } from "react";
 
 export const Experience = () => {
   return (
     <>
+      {/* <Suspense>
+        <Background />
+      </Suspense> */}
+      <Background />
+
       <OrbitControls />
       <mesh>
         <boxGeometry />
